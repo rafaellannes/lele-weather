eb361ea4ffb0bcad00b6a7648fb321c6f1695672
Rafael Lannes <rafaelvascolannes@gmail.com>
2026-01-30 22:49:51 -0300

    fix: force clear old service worker caches

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// Limpa todos os caches antigos do Service Worker
+if ('caches' in window) {
+  caches.keys().then(names => {
+    names.forEach(name => {
+      caches.delete(name)
+    })
+  })
+}
+
+// Força atualização do Service Worker se existir
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(registration => {
+      registration.update()
+    })
+  })
+}
+
 // Remove loading screen
 const loading = document.getElementById('loading')
 if (loading) {
